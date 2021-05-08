41fe0736b6fb3b561ce7b45dede86d9dfc59714e
manuel-192 <manuel-192@outlook.com>
2021-05-08 10:58:15 +0300

    .

--- pkg/pkglist_install.sh
+++ pkg/pkglist_install.sh
@@ -1,54 +1,57 @@
 #!/bin/bash
 
-Warning() {
-    echo "pkglist_install.sh: warning: $1"
-}
+Message() { echo "===> pkglist_install.sh: $1: $2" ; }
+
+Warning() { Message warning "$1" ; }
+Info()    { Message info "$1" ; }
 
 Main() {
     local pkglist="/home/liveuser/user_pkglist.txt"
 
-    if [ -f "$pkglist" ] ; then
-        local pkgs
-        local chroot_path=$(cat /tmp/chrootpath.txt)
+    if [ ! -f "$pkglist" ] ; then
+        Info "no $pkglist found."
+        return
+    fi
 
-        if [ -n "$(grep "Install=" $pkglist)" ] || [ -n "$(grep "Remove=" $pkglist)" ] ; then
+    local chroot_path=$(cat /tmp/chrootpath.txt)
 
-            # New: list of installs and removals, using array variables
-            #   - Install     Packages to be installed.
-            #   - Remove      Packages to be removed.
-            # File $pkglist must be using only bash syntax.
+    if [ -n "$(grep "^Install=" $pkglist)" ] || [ -n "$(grep "^Remove=" $pkglist)" ] ; then
 
-            local Install Remove
+        # New: list of installs and removals, using array variables
+        #   - Install     Packages to be installed.
+        #   - Remove      Packages to be removed.
+        # File $pkglist must be using only bash syntax.
 
-            source $pkglist || {
-                Warning "command 'source $pkglist' failed. Check $pkglist."
-                return 1
-            }
+        local Install Remove
 
-            if [ -n "$Remove" ] ; then
-                local Remove2=() pkg
-                for pkg in "${Remove[@]}" ; do
-                    if (pacman -Q --root $chroot_path "$pkg" >& /dev/null) ; then
-                        Remove2+=("$pkg")
-                    else
-                        Warning "package '$pkg' was not installed. Check $pkglist."
-                    fi
-                done
-                pacman -Rsn --noconfirm --root $chroot_path "${Remove2[@]}"
-            fi
-            if [ -n "$Install" ] ; then
-                if (! pacman -Sy --noconfirm --needed --root $chroot_path "${Install[@]}") ; then
-                    Warning "installing packages from $pkglist failed."
+        source $pkglist || {
+            Warning "command 'source $pkglist' failed. Check $pkglist."
+            return 1
+        }
+
+        if [ -n "$Remove" ] ; then
+            local Remove2=() pkg
+            for pkg in "${Remove[@]}" ; do
+                if (pacman -Q --root $chroot_path "$pkg" >& /dev/null) ; then
+                    Remove2+=("$pkg")
+                else
+                    Warning "package '$pkg' is not avaiable for uninstall. Check $pkglist."
                 fi
+            done
+            pacman -Rsn --noconfirm --root $chroot_path "${Remove2[@]}"
+        fi
+        if [ -n "$Install" ] ; then
+            if (! pacman -Sy --noconfirm --needed --root $chroot_path "${Install[@]}") ; then
+                Warning "installing packages from $pkglist failed."
             fi
-        else
+        fi
+    else
 
-            # Old: list of additional installs.
+        # Old: list of additional installs.
 
-            pkgs=$(echo $(cat $pkglist | sed 's|#.*||'))  # echo removes white spaces
-            if [ -n "$pkgs" ] ; then
-                pacman -Sy --noconfirm --needed --root $chroot_path $pkgs
-            fi
+        local pkgs=$(echo $(cat $pkglist | sed 's|#.*||'))  # echo removes white spaces
+        if [ -n "$pkgs" ] ; then
+            pacman -Sy --noconfirm --needed --root $chroot_path $pkgs
         fi
     fi
 }
